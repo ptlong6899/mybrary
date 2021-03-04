f54c7d7847590b39baec4a2eb5df216e26fdd198
10n9 <thanhlong6899@gmail.com>
2021-03-04 13:31:33 +0700

    init setup

---
+++ server.js
@@ -0,0 +1,47 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// mongoose
+//   .connect(MONGODB_URI, {
+//     // to get rid of deprecated warning
+//     // useCreateIndex: true,
+//     useNewUrlParser: true,
+//     // useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log(`Mongoose connected to ${MONGODB_URI}`);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000);
